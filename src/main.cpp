981803458ce8c6ba1aac0bfa34f5038e1c55a537
David Aliaga <aliaga@sensetime.jp>
2021-01-10 20:32:30 +0900

    going on one lane with smooth vel

--- src/main.cpp
+++ src/main.cpp
@@ -51,8 +51,13 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
+  //lane
+  int lane =1;
+  //reference velocity
+  double ref_vel= 49.5;
+
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+               &map_waypoints_dx,&map_waypoints_dy , &lane, &ref_vel]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -89,6 +94,9 @@ int main() {
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
+          int prev_size = previous_path_x.size();
+          //std::cout<<"Prev"<<prev_size<< std::endl;
+
           json msgJson;
 
           vector<double> next_x_vals;
@@ -102,12 +110,12 @@ int main() {
           //std::cout<<sensor_fusion<<std::endl;
 
           //First Straigth Path
-      /*     double dist_inc = 0.5;
+  /*         double dist_inc = 0.5;
           for (int i = 0; i < 50; ++i) {
                next_x_vals.push_back(car_x + (dist_inc*i)*cos(deg2rad(car_yaw)));
                next_y_vals.push_back(car_y + (dist_inc*i)*sin(deg2rad(car_yaw)));
            } 
-           */
+    */       
 
          //Circular Path
         /*  double pos_x;
@@ -144,7 +152,7 @@ int main() {
          */
 
           //Experiment with Frenet  
-          double dist_inc = 0.4;// 0.5;
+   /*       double dist_inc = 0.4;// 0.5;
           for (int i = 0; i < 50; ++i) {
              double next_s = car_s+(i+1)*(dist_inc);
              double next_d = 6; //middle lane
@@ -155,7 +163,7 @@ int main() {
              next_x_vals.push_back(next_point[0]);
              next_y_vals.push_back(next_point[1]);     
            }
-
+*/
 //Other
 /*
           double dist_inc = 0.5;
@@ -163,12 +171,123 @@ int main() {
                double next_s = car_s+(i+1)*dist_inc;
                double next_d = 6; 
                vector<double> xy= getXY(next_s,next_d,
-                                         max_waypoints_s,max_waypoints_x,max_waypoints_y);
+                                         map_waypoints_s,map_waypoints_x,map_waypoints_y);
 
                next_x_vals.push_back(xy[0]);
                next_y_vals.push_back(xy[1]);
            }
 */
+
+// spline
+
+          vector<double> ptsx;
+          vector<double> ptsy;
+
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);  // the angle in degrees
+
+
+
+          if(prev_size<2){  //use the car as starting reference
+           //use the two points that make the path tangent
+
+           double prev_car_x = car_x - cos(car_yaw);
+           double prev_car_y = car_y - sin(car_yaw);
+
+           ptsx.push_back(prev_car_x);
+           ptsx.push_back(car_x);
+
+           ptsy.push_back(prev_car_y);
+           ptsy.push_back(car_y);
+          }
+          else{  //use the previous path's end as starting reference
+             //Redefine refence state as previous path end point
+ 
+            ref_x = previous_path_x[prev_size-1];  //end point
+            ref_y = previous_path_y[prev_size-1];
+
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2];
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x - ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+
+          // Generate 3 (three) points 
+          // evenly spaced , ahead of the starting reference
+          vector<double> next_wp0 = getXY(car_s+30, (2+4*lane) ,
+                                          map_waypoints_s,map_waypoints_x,map_waypoints_y );
+          vector<double> next_wp1 = getXY(car_s+60, (2+4*lane) ,
+                                          map_waypoints_s,map_waypoints_x,map_waypoints_y );
+          vector<double> next_wp2 = getXY(car_s+90, (2+4*lane) ,
+                                          map_waypoints_s,map_waypoints_x,map_waypoints_y );                                                                
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+         //Now we do a transformation for the points to the car point of view
+
+         for(int i=0; i< ptsx.size(); i++){
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+
+            ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y* sin(0 - ref_yaw));
+            ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y* cos(0 - ref_yaw));
+         }
+
+         //spline
+         tk::spline s;
+         //add points
+         s.set_points(ptsx,ptsy);
+
+         //the we put the values in next_x_vals and next_y_vals
+         //first the previous points
+         for(int i=0;i<(previous_path_x.size());i++){
+           next_x_vals.push_back(previous_path_x[i]);
+           next_y_vals.push_back(previous_path_y[i]);
+         }
+
+         double target_x = 30.0;
+         double target_y = s(target_x);
+         double target_dist = distance(0,0,target_x,target_y);
+
+         double x_add_on= 0;
+
+         double N = (target_dist/(0.02*ref_vel/2.24));
+         double mini_target= target_x/N;
+
+         for(int i=0; i<= 50-previous_path_x.size();i++){
+               
+            //double N = (target_dist/(0.02*ref_vel/2.24));
+            double x_point = x_add_on +mini_target; //(target_x)/N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            //go to global coords
+            x_point =(x_ref *cos(ref_yaw)-y_ref*sin(ref_yaw));
+            y_point = (x_ref*sin(ref_yaw)+ y_ref* cos(ref_yaw));
+
+           x_point += ref_x;
+           y_point += ref_y;
+
+           next_x_vals.push_back(x_point);
+           next_y_vals.push_back(y_point);
+         }
+
 
 //END
 // passing
