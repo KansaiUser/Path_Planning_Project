1c1337f35be5b74d528abc1631de141366d70b22
David Aliaga <aliaga@sensetime.jp>
2021-01-18 21:40:15 +0900

    experiment change lane

--- src/main.cpp
+++ src/main.cpp
@@ -105,7 +105,7 @@ int main() {
           if(prev_size>0){
             //car_s = end_path_s;
             tip= end_path_s;
-            std::cout<<"car s: "<<car_s<<" tip "<<end_path_s<<std::endl;
+            std::cout<<"car s: "<<car_s<<" tip "<<end_path_s<<"  d "<<car_d<<std::endl;
           }
 
           bool too_close = false;
@@ -138,7 +138,10 @@ int main() {
 
                  //ref_vel=29.5;
                  too_close = true;
-
+                 ref_vel -= .224;
+                 if(lane>0){
+                   lane=0;
+                 }
 
                }
              }  // if car in our lane
@@ -178,48 +181,9 @@ int main() {
 
           //std::cout<<sensor_fusion<<std::endl;
 
-          //First Straigth Path
-  /*         double dist_inc = 0.5;
-          for (int i = 0; i < 50; ++i) {
-               next_x_vals.push_back(car_x + (dist_inc*i)*cos(deg2rad(car_yaw)));
-               next_y_vals.push_back(car_y + (dist_inc*i)*sin(deg2rad(car_yaw)));
-           } 
-    */       
+     
 
-         //Circular Path
-        /*  double pos_x;
-          double pos_y;
-          double angle;
-          int path_size = previous_path_x.size();
-
-          for (int i = 0; i < path_size; ++i) {
-              next_x_vals.push_back(previous_path_x[i]);
-              next_y_vals.push_back(previous_path_y[i]);
-           }
-
-          if (path_size == 0) {  //No previous points so get the value from localization
-                pos_x = car_x;
-                pos_y = car_y;
-                angle = deg2rad(car_yaw);
-            } 
-          else {  //previous points so get values from last point 
-            pos_x = previous_path_x[path_size-1];
-            pos_y = previous_path_y[path_size-1];
-
-            double pos_x2 = previous_path_x[path_size-2];
-            double pos_y2 = previous_path_y[path_size-2];
-            angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
-           }
-
-         double dist_inc = 0.5;
-         for (int i = 0; i < 50-path_size; ++i) {    
-                next_x_vals.push_back(pos_x+(dist_inc)*cos(angle+(i+1)*(pi()/100)));
-                next_y_vals.push_back(pos_y+(dist_inc)*sin(angle+(i+1)*(pi()/100)));
-                pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
-                pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
-           }
-         */
-
+   
           //Experiment with Frenet  
    /*       double dist_inc = 0.4;// 0.5;
           for (int i = 0; i < 50; ++i) {
